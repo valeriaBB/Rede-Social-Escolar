49375ec0a2e1e5a768a7e411e06d42f07ec29670
valeria banachi barreto <valeria banachi barreto>
2017-05-16 00:02:05 -0300

    bug responder enquete

--- TCC2/client/src/app/responderenquete/responderenquete.component.ts
+++ TCC2/client/src/app/responderenquete/responderenquete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { ServerService } from 'app/services/server/server.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-responderenquete',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./responderenquete.component.css']
 })
 export class ResponderenqueteComponent implements OnInit {
+  enquete = {};
+  constructor(private http: Http, private router: Router, private route: ActivatedRoute, private service: ServerService) {
+    var self = this;
+    this.route = route;
+    this.route.params.subscribe(params => {
+      var id = params['id'];
 
-  constructor() { }
+      if (id != undefined) {
+        this.buscaEnquete(id)
+          .subscribe(
+          res => self.enquete = res.json(),
+          erro => console.log(erro)
+          );
+      }
+    });
+    this.http = http;
+    console.log(this.enquete);
+    console.log(self.enquete);
+
+  }
 
   ngOnInit() {
+  }
+
+  salvar(event) {
+    this.service.salvar(this.enquete, 'enquete')
+      .subscribe(() => {
+        this.router.navigate(['/principal']);
+      }, erro => console.log(erro));
+  };
+
+  public buscaEnquete(id: string) {
+    return this.service.editar(id, 'enquete/');
   }
 
 }
